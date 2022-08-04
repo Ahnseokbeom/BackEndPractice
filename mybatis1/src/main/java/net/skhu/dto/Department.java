595031a1e33e3c1bf3d67f58d79f02b48ac37dda
seobkeom <ajm153123@naver.com>
2022-08-04 19:19:43 +0900

    mybatis1 commit

---
+++ mybatis1/src/main/java/net/skhu/dto/Department.java
@@ -0,0 +1,22 @@
+package net.skhu.dto;
+
+public class Department {
+	int id;
+	String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+}
